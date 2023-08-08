1bb41e3317e6b19c959fcab052bf8bf084a51219
omidashtari <oashtari@gmail.com>
2023-08-07 19:30:15 -0700

    added a player and its related values, position, rotation

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,17 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(200.0, 100.0);
+    player.rotation = std::f32::consts::FRAC_PI_2;
+    player.scale = 0.5;
     // set up game here
-
+    game.add_logic(game_logic);
     game.run((GameState::default()));
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // your actual game logic
+    // game_state.current_score += 1;
+    // println!("Current score: {}", game_state.current_score);
 }
